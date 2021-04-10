81fdaf2888911d663e3bb3753d577e020d481d88
MB <mariuszlodz@gmail.com>
2021-04-10 17:01:36 +0200

    upgrade: -LoanInformation page -method 'Calculation_monthly_cost_amound'

--- CLI/Controllers/HomeController.cs
+++ CLI/Controllers/HomeController.cs
@@ -30,9 +30,9 @@ namespace CLI.Controllers
             if (ModelState.IsValid)
             {
                 Annual_Interest_rate = parameters.Annual_Interest_rate == true ? 0.05 : 0.02;
+                Loan_amount = parameters.Loan_amount ?? 0.0;
                 Paid_administrative_fees = parameters.Administration_fee == true ? Calculation_administrative_fees() : 0.0;
                 Interest_rate_calculated_monthly = parameters.Interest_rate_calculated_monthly;
-                Loan_amount = parameters.Loan_amount ?? 0.0;
                 Duration_of_loan = parameters.Duration_of_loan ?? 0;
 
 
@@ -59,31 +59,32 @@ namespace CLI.Controllers
         private double Calculation_AOP()
         {
             double aop = 0.0;
-            double interest_value = 0.0;
+            double rate_sum = 0.0;
             var loan_value = Loan_amount + Paid_administrative_fees;
             var monthly_count = Duration_of_loan * 12;
 
             if (Interest_rate_calculated_monthly)
             {
                 var rate_value = loan_value / monthly_count;
+                var loan_parameter = loan_value / 12;
 
                 for (int counter = 1; monthly_count >= counter; counter++)
                 {
-                    interest_value += loan_value * (1 / 12) + loan_value * (1 / 12) * Annual_Interest_rate;
+                    rate_sum += loan_parameter + loan_parameter * Annual_Interest_rate;
                     loan_value -= rate_value;
                 }
 
-                aop = Annual_Interest_rate * interest_value / Loan_amount;
+                aop = 100 * rate_sum / Loan_amount - 100;
             }
             else
             {
                 for (int counter = 1; monthly_count >= counter; counter++)
                 {
-                    interest_value += 1 / Math.Pow(1 + Annual_Interest_rate / 12, counter);
+                    rate_sum += 1 / Math.Pow(1 + Annual_Interest_rate / 12, counter);
                 }
-
-                interest_value = (loan_value / interest_value) * monthly_count;
-                aop = Annual_Interest_rate * interest_value / Loan_amount;
+                //TODO: To small 
+                rate_sum = (loan_value / rate_sum) * monthly_count;
+                aop = 100 * rate_sum / Loan_amount - 100;
             }
 
             return aop;
@@ -97,11 +98,11 @@ namespace CLI.Controllers
 
             if (Interest_rate_calculated_monthly)
             {                
-                var rate_value = loan_value / monthly_count;              
-
+                var rate_value = loan_value / monthly_count;
+              
                 for (int counter = 1; monthly_count >= counter; counter++)
                 {
-                    interest_value += loan_value * (1 / 12) * Annual_Interest_rate;
+                    interest_value += loan_value / 12 * Annual_Interest_rate;
                     loan_value -= rate_value;
                 }
             }
@@ -119,7 +120,24 @@ namespace CLI.Controllers
         }
         private double Calculation_monthly_cost_amound()
         {           //TODO: make test when Loan_amount is 0
-            return Loan_amount * Annual_Interest_rate * 31 / 365;
+            var loan_value = Loan_amount + Paid_administrative_fees;
+
+            if (Interest_rate_calculated_monthly)
+            {
+                return loan_value / 12 + loan_value / 12 * Annual_Interest_rate;
+            }
+            else
+            {
+                double interest_value = 0.0;
+                var monthly_count = Duration_of_loan * 12;
+
+                for (int counter = 1; monthly_count >= counter; counter++)
+                {
+                    interest_value += 1 / Math.Pow(1 + Annual_Interest_rate / 12, counter);
+                }
+
+               return loan_value / interest_value;
+            }
         }
         private double Calculation_administrative_fees()
         {
